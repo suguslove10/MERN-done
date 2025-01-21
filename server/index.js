b01aec8778a8434d733de9b7b16a57b12d04979d
suguresh <sugugalag@gmail.com>
2025-01-22 01:29:10 +0530

    9

--- server/index.js
+++ server/index.js
@@ -10,7 +10,7 @@ const app = express();
 
 // Middleware
 app.use(cors({
-  origin: ['https://project-7-suguslove10-sugureshs-projects.vercel.app' , 'http://localhost:5173'],
+  origin: ['https://project-7-amber.vercel.app' , 'http://localhost:5173'],
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization']
